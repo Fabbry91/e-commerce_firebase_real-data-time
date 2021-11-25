54e186009a88e8869b96376c17c584770970a2ea
Fabricio <fabbry1991@gmail.com>
2021-11-25 19:07:50 -0300

    aplicacion de UseContext

--- Desktop/tp-obligatorio/src/App.js
+++ Desktop/tp-obligatorio/src/App.js
@@ -1,11 +1,23 @@
+import { useReducer } from 'react';
 import './App.css';
+import { AuthContext } from './Context/AuthContext';
+import { authReducer } from './Context/authReducer';
 import { RouterApp } from './router/RouterApp';
 
+const init = () => {
+  return JSON.parse(localStorage.getItem('user')) || { logged: false };
+}
+
 function App() {
+
+  const [user, dispatch] = useReducer(authReducer, {}, init)
+
   return (
-    <div className="App">
-      <RouterApp/>
-    </div>
+    <AuthContext.Provider value={{ user, dispatch }}>
+      <div className="App">
+        <RouterApp />
+      </div>
+    </AuthContext.Provider>
   );
 }
 
